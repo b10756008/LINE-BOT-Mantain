adebc5fb9818d283084a455313bd069529175097
b10756008 <50163520+b10756008@users.noreply.github.com>
2025-02-26 19:10:04 +0800

    Update web.py

--- web.py
+++ web.py
@@ -78,5 +78,5 @@ def download_file(filename):
     return send_file(os.path.join("uploads", filename), as_attachment=True)
 
 
-if __name__=='__main__':
-    app.run(host="0.0.0.0", port=5000)
+# if __name__=='__main__':
+#     app.run(host="0.0.0.0", port=5000)
